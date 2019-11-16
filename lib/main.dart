734260b3f25755abc55542bd82d66eb9c2c8fa60
Rajesh Kumar <rajeshkumar@HPS184.local>
2019-11-16 13:58:54 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web/widgets/waveAppBar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "28Days",
+        textTheme: Theme.of(context).textTheme.apply(fontFamily: "28Days", fontSizeFactor: 2.0),
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200),
+        child: LoopAppBar(
+          size: height * 0.3,
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 1200),
+          child: ListView(shrinkWrap: true, children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text("About"),
+                SizedBox(width: 100),
+                Text("Blog"),
+                SizedBox(width: 100),
+                Text("Contact"),
+              ],
+            ),
+
+            // Image.network("https://www.bikesportnews.com/uploads/news_images/MEYM_031119__AX28361.jpg"),
+            Container(
+              height: 350,
+              width: 350,
+              alignment: Alignment.centerLeft,
+              child: ClipOval(
+                child: Image.network(
+                  "https://firebasestorage.googleapis.com/v0/b/adventureweb-81601.appspot.com/o/DSC_6001%20rajesh__01.jpg?alt=media&token=de4cbd26-0255-497c-b840-041970800023",
+                  filterQuality: FilterQuality.medium,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Flutter| Dart2,\nReact-Native| JavaScript,\nSwift| iOS',
+                style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w900, letterSpacing: 1),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/waveAppBar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoopAppBar extends StatelessWidget {
+  const LoopAppBar({
+    Key key,
+    this.size,
+  }) : super(key: key);
+
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: size,
+      width: double.infinity,
+      child: ClipPath(
+        clipper: WaveClipper(),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, stops: [
+            0.0,
+            0.5,
+            1.0
+          ], colors: [
+            Colors.black87,
+            Colors.grey,
+            Colors.grey[300],
+            // Color(0xFF7074EC),
+            // Color(0xFF01C6FA),
+            // Color(0xFF00DDDE),
+          ])),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Flutter Adventure",
+                style: Theme.of(context).textTheme.copyWith(title: TextStyle(fontSize: 50)).title,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(0.0, size.height - size.height * 0.2);
+    //first magnet
+    var firstControlPoint = Offset(size.width / 4, size.height - size.height * 0.45);
+    var firstEndPoint = Offset(size.width / 2, size.height - size.height * 0.19);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
+
+    //Second magnet
+    var secondControlPoint = Offset(size.width * 0.75, size.height + size.height * 0.1);
+    var secondEndPoint = Offset(size.width, size.height - size.height * 0.2);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}

eb936e047cf2fe6f0abab9e9028d1241ee11c690
Rajesh Kumar <rajeshkumar@HPS184.local>
2019-11-17 23:29:36 +0530

    hosted

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Zmoke',
       theme: ThemeData(
         fontFamily: "28Days",
         textTheme: Theme.of(context).textTheme.apply(fontFamily: "28Days", fontSizeFactor: 2.0),
